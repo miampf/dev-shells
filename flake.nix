970dc63c5b73a1a3ccb62096c30cdb8830172477
miampf <miampf@proton.me>
2023-10-20 00:57:19 +0200

    added structure and rust-stable

--- flake.nix
+++ flake.nix
@@ -1,11 +1,37 @@
 {
-  description = "A very basic flake";
+  description = "Some shells for developing in specific environments.";
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
+
+  outputs = { self, nixpkgs, flake-utils }: 
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = (import nixpkgs) {
+          inherit system;
+        };
+        defaultInputs = [ pkgs.fish ];
+        defaultCommand = ''
+        echo "Time to mix drinks and change lives!"
+        fish
+        '';
+
+
+      in 
+      with pkgs;
+      {
+
+        devShells."rust-stable" = mkShell {
+          inherit system defaultInputs;
+          
+          buildInputs = defaultInputs ++ [ rustup rustc cargo ];
+
+          shellHook = ''
+          rustup toolchain add stable
+          '' + defaultCommand;
+        };
+      }
+    );
 }
